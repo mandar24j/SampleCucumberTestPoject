3e39a52e17571f190dffbd5162a5054fcb06b851
Mandar <edxsmtba@bbaaviation.net>
2021-05-24 11:44:12 +0530

    Chromedriver failure fix

--- src/test/java/utilities/PreSetup.java
+++ src/test/java/utilities/PreSetup.java
@@ -1,36 +1,36 @@
 package utilities;
 
-import java.io.FileInputStream;
-import java.util.Properties;
-import java.util.concurrent.TimeUnit;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 public class PreSetup {
-	public static WebDriver driver;
-	public static WebDriverWait wait;
-	public static Properties prop;
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+    public static Properties prop;
 
-	static {
-		try {
-			prop = new Properties();
-			prop.load(new FileInputStream("src//test//resources//configFiles//config.properties"));
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    static {
+        try {
+            prop = new Properties();
+            prop.load(new FileInputStream("src//test//resources//configFiles//config.properties"));
 
-	@BeforeClass
-	public static void launchApp() {
-		System.setProperty("webdriver.chrome.driver", prop.getProperty("chromeExe"));
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.get(prop.getProperty("appUrl"));
-		wait = new WebDriverWait(driver, 30);
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @BeforeClass
+    public static void launchApp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 30);
+    }
 }
--- src/test/resources/features/vacationScenarios.feature
+++ src/test/resources/features/vacationScenarios.feature
@@ -5,11 +5,11 @@ Feature: Vacation Tab Scenarios
     When user navigate to Vacation tab
     And user selects Flight, Hotel and "<Car>"
     And user enters "<origin>", "<destination>" locations
-    And user enters "<departingDaysFromToday>", "<departureTime>","<returtingDaysFromToday>", "<returningTime>"
+    And user enters "<departingDaysFromToday>", "<departureTime>","<returningDaysFromToday>", "<returningTime>"
     Then user gets atleast one search result
 
     Examples: 
-      | Car  | origin | destination | departingDaysFromToday | departureTime | returtingDaysFromToday | returningTime |
+      | Car  | origin | destination | departingDaysFromToday | departureTime | returningDaysFromToday | returningTime |
       | True | SFO    | LAX         |                      1 | evening       |                     21 | morning       |
       | True | SFO    | LAX         |                      2 | morning       |                     14 | morning       |
       | True | LAX    | NYC         |                      3 | evening       |                      7 | evening       |
